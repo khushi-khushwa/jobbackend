b910f1eed4de44bb316480e9f36b82fef6a080f8
khushi-khushwa <khushikhushwah70@gmail.com>
2024-10-21 14:43:09 +0530

    Your commit message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 // middleware
 app.use(express.json());
